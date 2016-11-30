0261c88d9c70ee13e01b68e6da7c530b6dba692d
Joey Marburger <joeymarburger@gmail.com>
2016-11-30 10:10:43 +0000

    Added a bunch of random files for the trust sidebar

---
+++ trust-project.js
@@ -0,0 +1,38 @@
+/*
+  File: trust-project.js
+  Description: Core javascript app structure for trust project app
+  Author(s):
+  Site: 
+*/
+
+// Setup
+var appCurrent = {
+  "key" : "item",
+  
+};
+
+// Init
+(function() {
+  // Article meta data
+  var articleMeta = {
+    
+  };
+  
+  // Look for data-trust attributes if not structured HTML
+  
+  
+  
+  // Get all non host domain links in body
+  
+  
+  
+  
+})();
+
+// Doc ready
+$(document).ready(function() {
+  
+  
+  
+  
+});
---
+++ trust-sidebar.js
@@ -0,0 +1,6 @@
+(function(){
+  /*var sideBar = document.createElement("div");
+  sideBar.id = "trust-sidebar";
+  sideBar.innerHTML = "<h1>Weeee!</h1>";
+  document.getElementsByTagName('body')[0].appendChild(sideBar);*/
+})();

c75332bb241ce6b3dddcfea582b20a10e94c56cc
Joey Marburger <joeymarburger@gmail.com>
2016-11-30 12:05:48 +0000

    Added font awesome and other crap, finished bookmarklet and sidebar js injection without content yet

--- trust-sidebar.js
+++
@@ -1,6 +0,0 @@
-(function(){
-  /*var sideBar = document.createElement("div");
-  sideBar.id = "trust-sidebar";
-  sideBar.innerHTML = "<h1>Weeee!</h1>";
-  document.getElementsByTagName('body')[0].appendChild(sideBar);*/
-})();
